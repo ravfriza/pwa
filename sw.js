ddcc9e32b74023e1b777b200df7d541119a82ff7
Daffa Ravfriza <daffa@hotelmurah.com>
2023-06-28 11:49:00 +0700

    update

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["./", "./src/master.css"]);
+            return cache.addAll(["./", "./src/master.css", "./img/bootstrap-logo.png"]);
         })
     );
 });
