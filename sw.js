a911e0b1a03620413d7c7d7c641ef0612637f9ce
Daffa Ravfriza <daffa@hotelmurah.com>
2023-06-28 11:21:07 +0700

    update

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
 self.addEventListener("install", e => {
-    console.log('install');
-})
+    e.waitUntil(
+        caches.open("static").then(cache => {
+            return cache.addAll(["./", "./src/master.css"]);
+        })
+    );
+});
+
+self.addEventListener("fetch", e => {
+    console.log(`Intercepting fetch request for: ${e.request.url}`);
+});
